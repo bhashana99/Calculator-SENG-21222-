226de93c940486297b791b7ad89c80de3e881963
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-10-18 23:27:43 +0530

    operator

--- src/java/uni/v2/Main.java
+++ src/java/uni/v2/Main.java
@@ -1,10 +1,18 @@
 package uni.v2;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
+
+    //SOLID principal
+    public static void main(String[] args) throws IOException {
 
         //Make sure to validate the arguments before using
-        if(args.length == 0 ){
+        if (args.length == 0) {
             System.out.println("Please Provide the operation as an argument");
             return;
         }
@@ -13,11 +21,32 @@ public class Main {
         //add, sub, mul, div
         String operator = args[0];
 
-        if (!(operator.equals("add") || operator.equals("sub") || operator.equals("mul") || operator.equals("div"))){
+        if (!(operator.equals("add") || operator.equals("sub") || operator.equals("mul") || operator.equals("div"))) {
             System.out.println("Please provide add,sub,mul or div as the operator argument");
             return;
         }
 
-        System.out.println(args[0]);
+        //read the number text file
+        List<String> numbersStr = Files.readAllLines(
+                Paths.get("D:\\UOK\\Academic\\Year 2\\constraction\\Calculator-SENG-21222-\\numbers.txt")
+        );
+
+        System.out.println(numbersStr.get(1));
+
+        double number1 = Double.parseDouble(numbersStr.get(0));
+        double number2 = Double.parseDouble(numbersStr.get(1));
+        double result = 0;
+
+        if (operator.equals("add")) {
+            result = number1 + number2;
+        } else if (operator.equals("sub")) {
+            result = number1 - number2;
+        } else if (operator.equals("mul")) {
+            result = number1 * number2;
+        } else if (operator.equals("div")) {
+            result = number1 / number2;
+        }
+
+        System.out.println(result);
     }
 }
